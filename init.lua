181a0b35f7b2f3b8e1703b6b091335ef47e02418
ruinb0w <ruinb0w@qq.com>
2023-04-21 23:47:37 +0800

    update config

--- init.lua
+++ init.lua
@@ -18,6 +18,24 @@ vim.keymap.set("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<
 vim.keymap.set("n", "<leader><leader>", "<Plug>(coc-diagnostic-next)", { silent = true })
 -- always show signcolumn
 vim.opt.signcolumn = "yes"
+-- Use K to show documentation in preview window
+function _G.show_docs()
+  local cw = vim.fn.expand('<cword>')
+  if vim.fn.index({ 'vim', 'help' }, vim.bo.filetype) >= 0 then
+    vim.api.nvim_command('h ' .. cw)
+  elseif vim.api.nvim_eval('coc#rpc#ready()') then
+    vim.fn.CocActionAsync('doHover')
+  else
+    vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
+  end
+end
+
+vim.keymap.set("n", "K", '<CMD>lua _G.show_docs()<CR>', { silent = true })
+-- Autocomplete
+function _G.check_back_space()
+  local col = vim.fn.col('.') - 1
+  return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+end
 
 --[[ coc-explorer ]]
 vim.keymap.set('n', '<leader>e', ':CocCommand explorer<CR>')
